db989e80231d14001d638405ec5e530b61d57c65
xingun <moon350703@gmail.com>
2021-12-17 10:02:03 +0700

    file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐 Hee loo 🖐<br>Bạn xinh gái !',
+    introDesc: `Tớ có điều này muốn hỏi bạn, bạn nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Noel này cậu có đi chơi với tớ khumm ? 😙',
+    desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu thích tớ đó nha :> ',
+    btnYes: 'Oke nha, tớ đồng ý 💖💖💖',
+    btnNo: '💔 cậu mơ à ! 💔 ',
+    question:'Cậu có gì muốn nói với tớ không 😊😊',
+    reasonPlaceholder: 'Lời nhắn iu thương',
+    btnReply: 'Gởi ! 💌💌',
+    reply: ' Ngày 25 này cậu ăn tối xong rồi qua đón tớ nhaaaaaa',
+    mess: 'Tớ biết mà, noel tới đón cậu nha 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho tớ nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://m.me/NgAnhTu873'
+}
